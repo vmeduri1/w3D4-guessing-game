ed5aa04c45b22e0b8b0fc0c6b28eb75fadf05c68
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-01-28 16:12:49 -0600

    First commit

---
+++ guessing-game.js
@@ -0,0 +1,35 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+let secretNumber = Number(23);
+askGuess();
+
+function checkGuess(num) {
+    if (num > secretNumber) {
+        console.log('Too high');
+        return false;
+    } else if (num < secretNumber) {
+        console.log('Too low');
+        return false;
+    } else {
+        console.log('Correct!');
+        return true;
+    }
+}
+
+function askGuess() {
+    rl.question('Enter a guess: ', (answer) => {
+        if (Number(answer) === secretNumber) {
+            console.log("You win!");
+            rl.close;
+        } else {
+            checkGuess(answer);
+            askGuess();
+        }
+      });
+}
+
+askGuess();
